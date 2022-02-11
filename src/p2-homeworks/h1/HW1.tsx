8dfba2a0d07800941b87710d32931a1af23ca4d0
Vitalex951 <vitalex951@gmail.com>
2022-02-11 11:20:28 +0300

    HW2 style

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -23,7 +23,7 @@ function HW1() {
                 time={messageData.time}
             />
 
-            <hr/>
+            {/*<hr/>*/}
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeMessage/>*/}
             {/*<hr/>*/}
--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import {AffairType} from "./HW2";
+import s from './Affairs.module.css';
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = () => {
+        console.log(props.affair._id)
+        return props.deleteAffairCallback(props.affair._id)
+    }// need to fix
 
     return (
         <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
+            <span>{props.affair.name}</span>
+            <span className={s.someClass}>{props.affair.priority}</span>
+            <button className={s.button} onClick={deleteCallback}>X</button>
         </div>
     )
 }
